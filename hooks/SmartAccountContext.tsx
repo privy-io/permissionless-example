c828a37a160ef881333ce22ea989ed0b75ff0327
bianc8.eth <48656276+bianc8@users.noreply.github.com>
2024-04-17 18:04:36 +0200

    update modules, fix pimlico, use baseSepolia, update NFT

--- hooks/SmartAccountContext.tsx
+++ hooks/SmartAccountContext.tsx
@@ -1,23 +1,31 @@
 import React, { useState, useEffect, useContext } from "react";
 import { ConnectedWallet, useWallets } from "@privy-io/react-auth";
+import { createPublicClient, createWalletClient, custom, http } from "viem";
+import { baseSepolia } from "viem/chains";
+import { Address, Chain, Transport } from "viem";
+import { SmartAccount } from "permissionless/accounts";
 import {
-  createPublicClient,
-  createWalletClient,
-  custom,
-  http,
-} from "viem";
-import { baseGoerli } from "viem/chains";
-import { walletClientToCustomSigner, type SmartAccountClient, createSmartAccountClient } from "permissionless";
+  walletClientToSmartAccountSigner,
+  type SmartAccountClient,
+  createSmartAccountClient,
+  ENTRYPOINT_ADDRESS_V06,
+} from "permissionless";
+import { EntryPoint } from "permissionless/types";
 import { signerToSimpleSmartAccount } from "permissionless/accounts";
 import { createPimlicoPaymasterClient } from "permissionless/clients/pimlico";
-import { BASE_GOERLI_ENTRYPOINT_ADDRESS, SMART_ACCOUNT_FACTORY_ADDRESS } from "../lib/constants";
+import { SMART_ACCOUNT_FACTORY_ADDRESS } from "../lib/constants";
 
 /** Interface returned by custom `useSmartAccount` hook */
 interface SmartAccountInterface {
   /** Privy embedded wallet, used as a signer for the smart account */
   eoa: ConnectedWallet | undefined;
   /** Smart account client to send signature/transaction requests to the smart account */
-  smartAccountClient: SmartAccountClient | undefined;
+  smartAccountClient:
+    | SmartAccountClient<EntryPoint, Transport, Chain, SmartAccount<EntryPoint>>
+    | Transport
+    | any
+    | SmartAccount<EntryPoint, string, Transport, Chain>
+    | null;
   /** Smart account address */
   smartAccountAddress: `0x${string}` | undefined;
   /** Boolean to indicate whether the smart account state has initialized */
@@ -41,14 +49,24 @@ export const SmartAccountProvider = ({
   children: React.ReactNode;
 }) => {
   // Get a list of all of the wallets (EOAs) the user has connected to your site
-  const {wallets} = useWallets();
+  const { wallets } = useWallets();
   // Find the embedded wallet by finding the entry in the list with a `walletClientType` of 'privy'
-  const embeddedWallet = wallets.find((wallet) => wallet.walletClientType === "privy");
+  const embeddedWallet = wallets.find(
+    (wallet) => wallet.walletClientType === "privy"
+  );
 
   // States to store the smart account and its status
   const [eoa, setEoa] = useState<ConnectedWallet | undefined>();
-  const [smartAccountClient, setSmartAccountClient] = useState<SmartAccountClient | undefined>();
-  const [smartAccountAddress, setSmartAccountAddress] = useState<`0x${string}` | undefined>();
+  const [smartAccountClient, setSmartAccountClient] = useState<
+    | SmartAccountClient<EntryPoint, Transport, Chain, SmartAccount<EntryPoint>>
+    | Transport
+    | any
+    | SmartAccount<EntryPoint, string, Transport, Chain>
+    | null
+  >();
+  const [smartAccountAddress, setSmartAccountAddress] = useState<
+    `0x${string}` | undefined
+  >();
   const [smartAccountReady, setSmartAccountReady] = useState(false);
 
   useEffect(() => {
@@ -60,32 +78,39 @@ export const SmartAccountProvider = ({
       const eip1193provider = await eoa.getEthereumProvider();
       const privyClient = createWalletClient({
         account: eoa.address as `0x${string}`,
-        chain: baseGoerli,
+        chain: baseSepolia,
         transport: custom(eip1193provider),
       });
 
-      const customSigner = walletClientToCustomSigner(privyClient);
+      const customSigner = walletClientToSmartAccountSigner(privyClient);
 
-      const publicClient = createPublicClient({ 
-        chain: baseGoerli, // Replace this with the chain of your app
-        transport: http()
-      })
-
-      const simpleSmartAccount = await signerToSimpleSmartAccount(publicClient, {
-        entryPoint: BASE_GOERLI_ENTRYPOINT_ADDRESS,
-        signer: customSigner,
-        factoryAddress: SMART_ACCOUNT_FACTORY_ADDRESS,
+      const publicClient = createPublicClient({
+        chain: baseSepolia, // Replace this with the chain of your app
+        transport: http(),
       });
 
+      const simpleSmartAccount = await signerToSimpleSmartAccount(
+        publicClient,
+        {
+          entryPoint: ENTRYPOINT_ADDRESS_V06,
+          signer: customSigner,
+          factoryAddress: SMART_ACCOUNT_FACTORY_ADDRESS! as Address,
+        }
+      );
+
       const pimlicoPaymaster = createPimlicoPaymasterClient({
+        chain: baseSepolia,
         transport: http(process.env.NEXT_PUBLIC_PIMLICO_PAYMASTER_URL),
+        entryPoint: ENTRYPOINT_ADDRESS_V06,
       });
 
       const smartAccountClient = createSmartAccountClient({
         account: simpleSmartAccount,
-        chain: baseGoerli, // Replace this with the chain for your app
-        transport: http(process.env.NEXT_PUBLIC_PIMLICO_BUNDLER_URL),
-        sponsorUserOperation: pimlicoPaymaster.sponsorUserOperation // If your app uses a paymaster for gas sponsorship
+        chain: baseSepolia, // Replace this with the chain for your app
+        bundlerTransport: http(process.env.NEXT_PUBLIC_PIMLICO_BUNDLER_URL),
+        middleware: {
+          sponsorUserOperation: pimlicoPaymaster.sponsorUserOperation, // If your app uses a paymaster for gas sponsorship
+        },
       });
 
       const smartAccountAddress = smartAccountClient.account?.address;
@@ -97,7 +122,6 @@ export const SmartAccountProvider = ({
 
     if (embeddedWallet) createSmartWallet(embeddedWallet);
   }, [embeddedWallet?.address]);
-
 
   return (
     <SmartAccountContext.Provider
